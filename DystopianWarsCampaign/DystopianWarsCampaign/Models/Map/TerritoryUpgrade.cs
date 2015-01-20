2706e67bfc17224bec830ac8d2e3c559b8f7ffe1
thomasverkooijen <info@thomasverkooijen.nl>
2015-01-20 14:09:08 +0100

    Testing out CF Database Generation.

---
+++ DystopianWarsCampaign/DystopianWarsCampaign/Models/Map/TerritoryUpgrade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DystopianWarsCampaign.Models.Map
+{
+    public class TerritoryUpgrade
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- DystopianWarsCampaign/DystopianWarsCampaign/Startup.cs
+++ DystopianWarsCampaign/DystopianWarsCampaign/Startup.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.Owin;
+﻿using DystopianWarsCampaign.Data_Access_Layer;
+using Microsoft.Owin;
 using Owin;
+using System.Data.Entity;
 
 [assembly: OwinStartupAttribute(typeof(DystopianWarsCampaign.Startup))]
 namespace DystopianWarsCampaign
 {
-    //Helllllo Thomas!
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            Database.SetInitializer<MapContext>(new MapInitializer());
         }
     }
 }
