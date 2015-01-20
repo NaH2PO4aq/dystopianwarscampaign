ff0f6a43f9be5d939bd971ec950987246e73b627
thomasverkooijen <info@thomasverkooijen.nl>
2015-01-20 14:09:42 +0100

    Revert "Testing out CF Database Generation."
    This reverts commit 2706e67bfc17224bec830ac8d2e3c559b8f7ffe1.

--- DystopianWarsCampaign/DystopianWarsCampaign/Startup.cs
+++ DystopianWarsCampaign/DystopianWarsCampaign/Startup.cs
@@ -1,17 +1,15 @@
-﻿using DystopianWarsCampaign.Data_Access_Layer;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
-using System.Data.Entity;
 
 [assembly: OwinStartupAttribute(typeof(DystopianWarsCampaign.Startup))]
 namespace DystopianWarsCampaign
 {
+    //Helllllo Thomas!
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            Database.SetInitializer<MapContext>(new MapInitializer());
         }
     }
 }
