eea9d1582a1ec2db82e92b1f8eb1ec2520ae62d5
sidrial <martens.stefan@gmail.com>
2015-01-21 22:10:48 +0100

    UpdateMAP

--- DystopianWarsCampaign/DystopianWarsCampaign/Properties/Resources.Designer.cs
+++ DystopianWarsCampaign/DystopianWarsCampaign/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace DystopianWarsCampaign.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap map2 {
+            get {
+                object obj = ResourceManager.GetObject("map2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap NONE {
             get {
                 object obj = ResourceManager.GetObject("NONE", resourceCulture);
