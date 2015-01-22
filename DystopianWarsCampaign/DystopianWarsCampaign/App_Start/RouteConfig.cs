73de18bd5dc2ad34e6102c6c859f3edb11a9c537
thomasverkooijen <info@thomasverkooijen.nl>
2015-01-22 21:45:42 +0100

    Added FactionController, updated About/Contact/Index pages.

--- DystopianWarsCampaign/DystopianWarsCampaign/App_Start/RouteConfig.cs
+++ DystopianWarsCampaign/DystopianWarsCampaign/App_Start/RouteConfig.cs
@@ -14,10 +14,17 @@ namespace DystopianWarsCampaign
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Admin",
+                url: "admin/{controller}/{action}/{id}"
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
