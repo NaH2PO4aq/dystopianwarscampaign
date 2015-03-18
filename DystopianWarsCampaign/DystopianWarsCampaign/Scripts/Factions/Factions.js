f0a02a97d02918f6efe714132b21231b0450e063
thomasverkooijen <info@thomasverkooijen.nl>
2015-03-18 14:47:30 +0100

    Fixed js

---
+++ DystopianWarsCampaign/DystopianWarsCampaign/Scripts/Factions/Factions.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    function tally(selector) {
+        $(selector).each(function () {
+            var total = 0,
+                column = $(this).siblings(selector).andSelf().index(this);
+            $(this).parents().prevUntil(':has(' + selector + ')').each(function () {
+                total += parseFloat($('td.sum:eq(' + column + ')', this).html()) || 0;
+            })
+            $(this).html(total);
+        });
+    }
+    tally('td.subtotal');
+});
